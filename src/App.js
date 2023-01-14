73f4f2d466af3d7fc359dfd35bd0e20a22bb857c
Hassan Alaa Lasheen <hlasheen21@gmail.com>
2023-01-14 14:32:42 +0200

    Login page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
