567f99e7698032ee3516a42951b2a03d926e5e09
Hassan Lasheen <106620966+Hassanlasheenn@users.noreply.github.com>
2023-01-17 19:29:31 +0200

    Revert "Merge pull request #1 from Hassanlasheenn/master"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Login title="Log in | Contra" />} />
-        <Route exact path='/sign-up' element={<Signup title="Sign up | Contra" />} />
       </Routes>
     </div>
   );
