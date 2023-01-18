6da52146ff6d6d737e50c08a3d7035f62580bcb6
Hassan Alaa Lasheen <hlasheen21@gmail.com>
2023-01-18 22:07:58 +0200

    Added Material UI instead of the normal JSX components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import "./sass/main.scss";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Login title="Log in | Contra" />} />
+        <Route
+          exact
+          path="/log-in"
+          element={<Login title="Log in | Contra" />}
+        />
       </Routes>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import { Typography, Button, Box, Card } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import React, { useEffect } from "react";
 import blur from "../assets/blur_bg_3.jpg";
 import LogoContra from "../assets/LogoContra";
@@ -7,44 +9,148 @@ const Login = ({ title }) => {
     document.title = title;
   });
 
+  const theme = createTheme({
+    shadows: ["none"],
+  });
+
   return (
-    <div className="login">
-      <div className="login__bg">
-        <img className="login__back-bg" src={blur} alt="" />
-      </div>
-      <div className="login__banner">
+    <Box className="login">
+      <Box className="login__bg">
+        <Box
+          component="img"
+          sx={{
+            width: "100%",
+            height: "100vh",
+            filter: "blur(2rem)",
+          }}
+          className="login__back-bg"
+          src={blur}
+          alt=""
+        />
+      </Box>
+      <Box className="login__banner">
         <LogoContra />
-      </div>
+      </Box>
 
-      <div className="login__page">
-        <div className="login__container">
-          <div className="login__container-left">
-            <h1 className="login__container-left--title">
+      <Box className="login__page">
+        <Card
+          sx={{
+            boxSizing: "border-box",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            bottom: "15em",
+            right: "24em",
+            background: "white",
+            borderRadius: "2rem",
+            boxShadow: "none",
+            width: "70%",
+            height: "70vh",
+            paddingLeft: "6em",
+          }}
+        >
+          <Box className="login__container-left">
+            <Typography
+              variant="h2"
+              sx={{
+                fontSize: "3.125rem",
+                fontWeight: 700,
+                letterSpacing: "0.5px",
+                lineHeight: "40px",
+                marginBottom: "1em",
+              }}
+            >
               Welcome back to Contra 👋
-            </h1>
-            <div className="login__container-left--input-cont">
-              <label>Email address</label>
+            </Typography>
+            <Box className="login__container-left--input-cont">
+              <Typography
+                sx={{
+                  position: "absolute",
+                  top: ".5rem",
+                  left: "1.45rem",
+                  fontSize: "1.1rem",
+                  fontWeight: 400,
+                  lineHeight: 1.333,
+                  color: "rgb(103, 112, 132)",
+                }}
+              >
+                Email address
+              </Typography>
               <input type="text" placeholder="name@email.com" required />
-            </div>
-            <button className="login__container-left--btn">Log In</button>
-          </div>
-          <div className="login__container-right">
-            <img
+            </Box>
+            <Button
+              variant="contained"
+              sx={{
+                padding: ".8rem 1.4rem",
+                backgroundColor: "black",
+                border: "none",
+                color: "white",
+                borderRadius: "2.5rem",
+                fontWeight: "600",
+                fontSize: "1.2rem",
+                cursor: "pointer",
+                textTransform: "none",
+                "&:hover": {
+                  backgroundColor: "black",
+                },
+              }}
+            >
+              Log In
+            </Button>
+          </Box>
+          <Box className="login__container-right">
+            <Box
+              component="img"
+              sx={{
+                width: "90%",
+              }}
               src="https://contra.com/static/assets/login@2x.b44e5924.webp"
-              alt="Contra"
-              className="login__container-right--image"
+              alt="Contra star house"
             />
-          </div>
-        </div>
+          </Box>
+        </Card>
 
-        <div className="signup__container">
-          <p className="signup__container-text">
+        <Box className="signup__container">
+          <Typography
+            variant="h5"
+            sx={{
+              color: "grey.dark",
+              maxWidth: "480px",
+              textAlign: "center",
+              fontWeight: "400",
+              fontSize: "1.7rem",
+              letterSpacing: ".15px",
+              lineHeight: " 1.42",
+            }}
+          >
             New to Contra? Join our commision-free platform
-          </p>
-          <button className="signup__container-btn">Sign Up</button>
-        </div>
-      </div>
-    </div>
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              padding: "1rem 1.3rem",
+              backgroundColor: "transparent",
+              border: "1px solid black",
+              color: "black",
+              borderRadius: "2.5rem",
+              fontWeight: "600",
+              fontSize: "1.2rem",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: "transparent",
+                color: "white",
+              },
+            }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
